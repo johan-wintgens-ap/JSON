a8e49c5ce4b42cb33ae4aae3fc6a3d7338364712
johanwintgens <peoplecallmeshady@hotmail.com>
2015-05-24 20:57:48 +0200

    init

---
+++ JSON/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JSON;
+using Newtonsoft.Json;
+
+namespace JSON_Data
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string url = "http://datasets.antwerpen.be/v4/gis/repetitieruimteoverzicht.json";
+
+            WebClient wc = new WebClient();
+            string jsondata = wc.DownloadString(url);
+
+            Rootobject data = JsonConvert.DeserializeObject<Rootobject>(jsondata);
+            List<JSON.Datum> RepetitieRuimtes = new List<Datum>();
+            bool unique;
+            foreach (var datum in data.data)
+            {
+                unique = true;
+                for (int i = 0; i < RepetitieRuimtes.Count; i++)
+                {
+                    if (RepetitieRuimtes[i].id == datum.id && unique)
+                        unique = false;
+                }
+                if (unique)
+                    RepetitieRuimtes.Add(datum);
+            }
+        }
+    }
+}
